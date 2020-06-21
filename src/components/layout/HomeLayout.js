64d91f89fbaf5480636ad6414de7ce058ae15525
Bùi Tuấn <tuan.bk996@gmail.com>
2020-06-22 00:06:51 +0700

    update chat

--- src/components/layout/HomeLayout.js
+++ src/components/layout/HomeLayout.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Row, Col } from 'antd';
 import Utinities from "../commoms/Utinities"
 import UserBox from "../commoms/UserBox"
+import ChatPanel from "../commoms/ChatPanel";
 
 const HomeLayout = (props) => {
 	return (
@@ -12,6 +13,7 @@ const HomeLayout = (props) => {
 					<UserBox></UserBox>
 					<Utinities></Utinities>
 				</Col>
+				<ChatPanel></ChatPanel>
 			</Row>
 		</div>
 	);
